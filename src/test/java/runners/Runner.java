09991b18e45b5539a65d301031a6f872b9235a9d
seymalacin <seymallacinn@gmail.com>
2023-03-08 20:15:26 +0300

    08.03.23

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class) //test çalıştırıcı notasyonu
 @CucumberOptions(  // Seneryoların nerede ve nasıl çalışacağı,hangi raporu kullanılmasıyla alakalı seçenekleri ayarlar
-        plugin = {"html:src/test/resources/reports/gp2.html"},
+        plugin = {"html:src/test/resources/reports/g1g2g3g4.html"},
         features = "src/test/resources/features", // features'ın yolunu belirmemiz gerekir
         glue = "stepdefinitions",// Kodlarımızı yazdığımız yeri belirtiriz
         tags =  "@g1 or @g2 or @g3 or @g4 ", //Çalıştırmak istediğimiz seneryoya verdiğimiz tag'i belirtiriz
